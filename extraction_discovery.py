f6e13b8b4d13203e61fcb807278e9711f123e3ee
Dane Grote <dane.a.grote@gmail.com>
2023-08-04 22:01:42 -0500

    Update extraction_discovery.py
    More work on auto populate form. deploy for further model testing

--- extraction_discovery.py
+++ extraction_discovery.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 import pandas as pd
 import json
+import datetime
 
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import create_extraction_chain, create_extraction_chain_pydantic
@@ -45,11 +46,11 @@ def load_chain():
 chain = load_chain()
 
 st.set_page_config(page_title="Project Louru | Alpha Extractor", page_icon=":robot:")
-st.header("Project Louru | Alpha Extractor")
+st.header("Experience Manager | Project Louru")
 st.markdown(""" 
     Welcome to the **Louru Experience Manager** (Alpha)! To add your upcoming experience, simply provide its name, date, time, location, a brief description, and any other relevant details. 
 
-    For example: "Post Malone is playing tonight at The Broadway Oyster Bar. Show starts at 7:00 PM, and there is a $10 cover.
+    For example: "Post Malone is playing tonight at The Broadway Oyster Bar. Show starts at 7:00 PM, and there is a $10 cover."
 
     Let's share your event with the community and make it a memorable experience for everyone!
 """)
@@ -64,19 +65,41 @@ user_input = get_text()
 
 if user_input:
     output = chain.run(input=user_input)
-    df = pd.DataFrame(output).dropna(axis=1).iloc[0]
+    df = pd.DataFrame(output).iloc[0]
 
     with st.chat_message("user"):
         st.write("Does the below look correct? If so press submit")
         st.write("If changes are needed, please make them before submitting.")
 
-        st.data_editor(df)
+        #st.data_editor(df)
 
         with st.form('confirmationForm'):
             st.text_input(value=df['business_name'], label='Business Name')
             st.text_input(value=df['event_type'], label='Experience Type')
             st.number_input(value=df['event_price'], label='Experience Price', min_value=0)
 
+            if df['band_name']:
+                st.text_input(value=df['band_name'], label='Band Name')
+
+            if df['event_date']:
+                st.date_input(value=datetime.date(2019, 7, 6), label='Experience Date')
+
+            if df['event_start_time']:
+                st.time_input(value=datetime.time(19, 0), label='Experience Start Time')
+
+            if df['event_end_time']:
+                st.time_input(value=datetime.time(22, 0), label='Experience End Time')
+
+            if df['happy_hour_start_time']:
+                st.time_input(value=datetime.time(19, 0), label='Happy Hour Start Time')
+
+            if df['happy_hour_end_time']:
+                st.time_input(value=datetime.time(22, 0), label='Happy Hour End Time')
+
+            if df['happy_hour_deal']:
+                    st.text_input(value=df['happy_hour_deal'], label='Happy Hour Deal')
+
             submit = st.form_submit_button('Submit')
 
-
+            if submit:
+                'Thank you!'
